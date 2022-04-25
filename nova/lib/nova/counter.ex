72ab96333797e3e579e081d5fb4f753b0d2e4d8f
Yas <yas@yas.nyc>
2022-04-25 14:59:41 -0400

    adding read and return count

--- nova/lib/nova/counter.ex
+++ nova/lib/nova/counter.ex
@@ -1,30 +1,37 @@
-defmodule Nova.Counter do 
+defmodule Nova.Counter do
   @moduledoc """
   Documentation for `Counter`.
   """
 
+  @doc """
+   New - creates the new map with the string as an integer
+
+  """
+  def new(input) when is_binary(input) do
+    %{count: String.to_integer(input)}
+  end
 
   @doc """
-  New - creates the new map with the string as an integer
+   Inc - increments the count in the map
 
-	"""
-	def new(input) when is_binary(input) do 
-		%{count: String.to_integer(input)}
-	end
-
-  @doc """
-  Inc - increments the count in the map
-
-	"""
+  """
   def inc(acc) do
     %{count: Map.get(acc, :count) + 1}
   end
 
   @doc """
-  Message - prints out the count in the map
+   Dec - decrements the count in the map
 
-	"""
-	def message(acc) do 
+  """
+  def dec(acc) do
+    %{count: Map.get(acc, :count) - 1}
+  end
+
+  @doc """
+   Message - prints out the count in the map
+
+  """
+  def message(acc) do
     "The count is #{acc.count}"
   end
 end
--- nova/lib/nova/service.ex
+++ nova/lib/nova/service.ex
@@ -1,9 +1,25 @@
-defmodule Nova.Service do 
-	alias Nova.Counter
-	def listen(counter) do  
-		receive do
-			:inc -> 
-				Counter.inc(counter)
-		end
-	end
+defmodule Nova.Service do
+  alias Nova.Counter
+
+  def start(string) do
+    spawn(fn -> string |> Counter.new() |> run() end)
+  end
+
+  def run(counter) do
+    counter |> listen() |> run()
+  end
+
+  def listen(counter) do
+    receive do
+      :inc ->
+        Counter.inc(counter)
+
+      :dec ->
+        Counter.dec(counter)
+
+      {:read, from} ->
+        send(from, Counter.message(counter))
+        counter
+    end
+  end
 end
