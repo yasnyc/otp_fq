2d428549f6aa06fd4c8a8f9253ab4d180c4fe75e
Eduardo Borsa <borsa.dado@gmail.com>
2022-04-25 12:13:00 -0300

    Add the many counter key task

--- nova/lib/nova.ex
+++ nova/lib/nova.ex
@@ -1,18 +1,29 @@
 defmodule Nova do
-  @moduledoc """
-  Documentation for `Nova`.
-  """
+  def new() do
+    %{}
+  end
 
-  @doc """
-  Hello world.
+  def add_key(acc, key) do
+    acc
+    |> Map.put(key, 0)
+  end
 
-  ## Examples
+  # This is an accumulator
+  def inc_key(acc, key) do
+    if Map.keys(acc) |> Enum.member?(key) do
+      Map.get_and_update(acc, key, fn current_value ->
+        {:ok, current_value + 1}
+      end)
+      |> elem(1)
+    else
+      acc
+    end
+  end
 
-      iex> Nova.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def message(acc) do
+    acc
+    |> Enum.map(fn {key, value} ->
+      "The count of #{key} is #{value}!"
+    end)
   end
 end
